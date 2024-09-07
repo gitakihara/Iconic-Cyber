a5b3766f141a4604c15ac53781bcdb34f05fc1a8
gitakihara <132453040+gitakihara@users.noreply.github.com>
2024-09-07 19:14:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+    const name = document.querySelector('input[name="name"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const message = document.querySelector('textarea').value;
+  
+    if (name === "" || email === "" || message === "") {
+      e.preventDefault();
+      alert('Please fill out all fields.');
+    }
+  });
